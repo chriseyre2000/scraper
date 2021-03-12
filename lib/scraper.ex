1a823e3eb04b5e6d4b79f2e450ad1fd8c47bcb1c
Christopher Eyre <chriseyre2000@gmail.com>
2021-03-12 19:17:58 +0000

    Initial version

---
+++ lib/scraper.ex
@@ -0,0 +1,14 @@
+defmodule Scraper do
+  @moduledoc """
+  Documentation for `Scraper`.
+  """
+
+  def work() do
+    # For simplicity, this function is just a placeholder
+    # and does not contain any real scraper logic
+    1..5
+    |> Enum.random()
+    |> :timer.seconds()
+    |> Process.sleep()
+  end
+end
